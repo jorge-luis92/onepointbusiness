f79444dbdfcb8dedc6c55f7de531596699015946
unknown <hdzv.jorgeluis@gmail.com>
2022-05-03 16:46:09 -0500

    Post

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://onepointbussiness.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://onepointbussiness.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
