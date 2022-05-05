9e206ee19d0a2a2aa32a6749dea1d26b1eb348cf
unknown <hdzv.jorgeluis@gmail.com>
2022-05-04 20:06:58 -0500

    force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    
+        //
+    }
 }
